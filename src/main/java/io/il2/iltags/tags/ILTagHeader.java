9f74a057eb2f6e4ce585ef1edc6de517fe456fae
Fabio Jun Takada Chino <fchino@il2.io>
2022-06-02 23:24:26 -0300

    ILTagHeader added.

---
+++ src/main/java/io/il2/iltags/tags/ILTagHeader.java
@@ -0,0 +1,167 @@
+/*
+ * BSD 3-Clause License
+ * 
+ * Copyright (c) 2021-2022, InterlockLedger
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package io.il2.iltags.tags;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import io.il2.iltags.ilint.ILIntDecoder;
+import io.il2.iltags.ilint.ILIntEncoder;
+
+/**
+ * This helper class implements the ILTag header format. It provides methods
+ * that can help with the serialization and deserialization of the ILTag
+ * headers.
+ *
+ * @author Fabio Jun Takada Chino
+ * @since 2022.06.02
+ */
+public class ILTagHeader {
+
+	/**
+	 * The tag id.
+	 */
+	public long tagId;
+
+	/**
+	 * The value size.
+	 */
+	public long valueSize;
+
+	/**
+	 * Creates a new instance of this class.
+	 */
+	public ILTagHeader() {
+	}
+
+	/**
+	 * Creates a new instance of this class.
+	 * 
+	 * @param tagId     The tag id.
+	 * @param valueSize Size of the value. It is ignored for implicit tags.
+	 */
+	public ILTagHeader(long tagId, long valueSize) {
+		this.tagId = tagId;
+		this.valueSize = valueSize;
+	}
+
+	/**
+	 * Returns the size of the serialized header.
+	 * 
+	 * @return The size of the header in bytes.
+	 */
+	public long getSerializedSize() {
+		return getSerializedSize(tagId, valueSize);
+	}
+
+	/**
+	 * Returns the size of the serialized header.
+	 * 
+	 * @param id        The tag id.
+	 * @param valueSize Size of the value. It is ignored for implicit tags.
+	 * @return The size of the header in bytes.
+	 */
+	public static long getSerializedSize(long id, long valueSize) {
+		long headerSize = ILIntEncoder.encodedSize(id);
+		if (!TagID.isImplicit(id)) {
+			headerSize += ILIntEncoder.encodedSize(valueSize);
+		}
+		return headerSize;
+	}
+
+	/**
+	 * Serializes the header.
+	 * 
+	 * @param out The data output.
+	 * @throws IOException In case of IO error.
+	 */
+	public void serialize(DataOutput out) throws IOException {
+		serialize(tagId, valueSize, out);
+	}
+
+	/**
+	 * Serializes the header.
+	 * 
+	 * @param id        The tag id.
+	 * @param valueSize Size of the value. It is ignored for implicit tags.
+	 * @param out       The data output.
+	 * @throws IOException In case of IO errors.
+	 */
+	public static void serialize(long id, long valueSize, DataOutput out) throws IOException {
+		ILIntEncoder.encode(id, out);
+		if (!TagID.isImplicit(id)) {
+			ILIntEncoder.encode(valueSize, out);
+		}
+	}
+
+	/**
+	 * Deserializes the header. For implicit tags the value size will be determined
+	 * by io.il2.iltags.tags.TagID.getImplicitValueSize(long).
+	 * 
+	 * @param in the data input.
+	 * @throws IOException    In case of IO error.
+	 * @throws ILTagException In case the header is corrupted.
+	 */
+	public void deserialize(DataInput in) throws IOException, ILTagException {
+		try {
+			this.tagId = ILIntDecoder.decode(in);
+		} catch (IllegalArgumentException e) {
+			throw new CorruptedTagException("Invalid tag id.");
+		}
+		if (TagID.isImplicit(this.tagId)) {
+			this.valueSize = TagID.getImplicitValueSize(this.tagId);
+		} else {
+			try {
+				this.valueSize = ILIntDecoder.decode(in);
+				AbstractILTag.assertTagSizeLimit(this.valueSize);
+			} catch (IllegalArgumentException e) {
+				throw new CorruptedTagException("Invalid tag value size.");
+			}
+		}
+	}
+
+	/**
+	 * Deserializes the header. For implicit tags the value size will be determined
+	 * by io.il2.iltags.tags.TagID.getImplicitValueSize(long).
+	 * 
+	 * @param in the data input.
+	 * @return The header.
+	 * @throws IOException    In case of IO error.
+	 * @throws ILTagException In case the header is corrupted.
+	 */
+	public static ILTagHeader deserializeHeader(DataInput in) throws IOException, ILTagException {
+		ILTagHeader header = new ILTagHeader();
+		header.deserialize(in);
+		return header;
+	}
+}
---
+++ src/test/java/io/il2/iltags/tags/ILTagHeaderTest.java
@@ -0,0 +1,278 @@
+/*
+ * BSD 3-Clause License
+ * 
+ * Copyright (c) 2021-2022, InterlockLedger
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package io.il2.iltags.tags;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.nio.ByteBuffer;
+
+import org.junit.jupiter.api.Test;
+
+import io.il2.iltags.ilint.ILIntEncoder;
+import io.il2.iltags.io.ByteBufferDataInput;
+
+class ILTagHeaderTest {
+
+	private static final long[] SAMPLE_IDS = { 0x10, 0xFEl, 0xFEDCl, 0xFEDCBAl, 0xFEDCBA98l, 0xFEDCBA9876l,
+			0xFEDCBA987654l, 0xFEDCBA98765432l, 0xFEDCBA9876543210l };
+
+	private static final long[] SAMPLE_SIZES = { 0x20L, 0x2000L, 0x200000L, 0x20000000L };
+
+	@Test
+	void testILTagHeader() {
+		ILTagHeader h = new ILTagHeader();
+		assertEquals(0, h.tagId);
+		assertEquals(0, h.valueSize);
+	}
+
+	@Test
+	void testILTagHeaderLongLong() {
+		ILTagHeader h = new ILTagHeader(1, 2);
+		assertEquals(1, h.tagId);
+		assertEquals(2, h.valueSize);
+	}
+
+	@Test
+	void testGetSerializedSize() {
+		// Implicit
+		for (long id = 0; id < 16; id++) {
+			ILTagHeader h = new ILTagHeader(id, -1);
+			assertEquals(ILIntEncoder.encodedSize(id), h.getSerializedSize());
+		}
+		// Explicit
+		for (long id : SAMPLE_IDS) {
+			for (long size : SAMPLE_IDS) {
+				ILTagHeader h = new ILTagHeader(id, size);
+				assertEquals(ILIntEncoder.encodedSize(id) + ILIntEncoder.encodedSize(size), h.getSerializedSize());
+			}
+		}
+	}
+
+	@Test
+	void testGetSerializedSizeLongLong() {
+		// Implicit
+		for (long id = 0; id < 16; id++) {
+			assertEquals(ILIntEncoder.encodedSize(id), ILTagHeader.getSerializedSize(id, -1));
+		}
+		// Explicit
+		for (long id : SAMPLE_IDS) {
+			for (long size : SAMPLE_IDS) {
+				assertEquals(ILIntEncoder.encodedSize(id) + ILIntEncoder.encodedSize(size),
+						ILTagHeader.getSerializedSize(id, size));
+			}
+		}
+	}
+
+	@Test
+	void testSerializeDataOutput() throws Exception {
+
+		// Implicit
+		for (long id = 0; id < 16; id++) {
+			ILTagHeader h = new ILTagHeader(id, -1);
+
+			ByteArrayOutputStream bExp = new ByteArrayOutputStream();
+			try (DataOutputStream out = new DataOutputStream(bExp)) {
+				ILIntEncoder.encode(id, out);
+			}
+			ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+			try (DataOutputStream out = new DataOutputStream(bOut)) {
+				h.serialize(out);
+			}
+			assertArrayEquals(bExp.toByteArray(), bOut.toByteArray());
+		}
+
+		// Implicit
+		for (long id : SAMPLE_IDS) {
+			for (long size : SAMPLE_IDS) {
+				ILTagHeader h = new ILTagHeader(id, size);
+
+				ByteArrayOutputStream bExp = new ByteArrayOutputStream();
+				try (DataOutputStream out = new DataOutputStream(bExp)) {
+					ILIntEncoder.encode(id, out);
+					ILIntEncoder.encode(size, out);
+				}
+				ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+				try (DataOutputStream out = new DataOutputStream(bOut)) {
+					h.serialize(out);
+				}
+				assertArrayEquals(bExp.toByteArray(), bOut.toByteArray());
+			}
+		}
+	}
+
+	@Test
+	void testSerializeLongLongDataOutput() throws Exception {
+
+		// Implicit
+		for (long id = 0; id < 16; id++) {
+			ByteArrayOutputStream bExp = new ByteArrayOutputStream();
+			try (DataOutputStream out = new DataOutputStream(bExp)) {
+				ILIntEncoder.encode(id, out);
+			}
+			ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+			try (DataOutputStream out = new DataOutputStream(bOut)) {
+				ILTagHeader.serialize(id, -1, out);
+			}
+			assertArrayEquals(bExp.toByteArray(), bOut.toByteArray());
+		}
+
+		// Implicit
+		for (long id : SAMPLE_IDS) {
+			for (long size : SAMPLE_IDS) {
+				ByteArrayOutputStream bExp = new ByteArrayOutputStream();
+				try (DataOutputStream out = new DataOutputStream(bExp)) {
+					ILIntEncoder.encode(id, out);
+					ILIntEncoder.encode(size, out);
+				}
+				ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+				try (DataOutputStream out = new DataOutputStream(bOut)) {
+					ILTagHeader.serialize(id, size, out);
+				}
+				assertArrayEquals(bExp.toByteArray(), bOut.toByteArray());
+			}
+		}
+	}
+
+	@Test
+	void testDeserialize() throws Exception {
+		ILTagHeader h = new ILTagHeader();
+
+		// Implicit
+		for (long id = 0; id < 16; id++) {
+			ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+			try (DataOutputStream out = new DataOutputStream(bOut)) {
+				ILIntEncoder.encode(id, out);
+			}
+			ByteBufferDataInput in = new ByteBufferDataInput(ByteBuffer.wrap(bOut.toByteArray()));
+			h.deserialize(in);
+			assertEquals(id, h.tagId);
+			assertEquals(TagID.getImplicitValueSize(id), h.valueSize);
+		}
+
+		// General
+		for (long id : SAMPLE_IDS) {
+			for (long size : SAMPLE_SIZES) {
+				ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+				try (DataOutputStream out = new DataOutputStream(bOut)) {
+					ILIntEncoder.encode(id, out);
+					ILIntEncoder.encode(size, out);
+				}
+				ByteBufferDataInput in = new ByteBufferDataInput(ByteBuffer.wrap(bOut.toByteArray()));
+				h.deserialize(in);
+				assertEquals(id, h.tagId);
+				assertEquals(size, h.valueSize);
+			}
+		}
+
+		for (long id : SAMPLE_IDS) {
+			ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+			try (DataOutputStream out = new DataOutputStream(bOut)) {
+				ILIntEncoder.encode(id, out);
+				ILIntEncoder.encode(ILTag.MAX_TAG_SIZE, out);
+			}
+			ByteBufferDataInput in = new ByteBufferDataInput(ByteBuffer.wrap(bOut.toByteArray()));
+			h.deserialize(in);
+			assertEquals(id, h.tagId);
+			assertEquals(ILTag.MAX_TAG_SIZE, h.valueSize);
+		}
+
+		for (long id : SAMPLE_IDS) {
+			ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+			try (DataOutputStream out = new DataOutputStream(bOut)) {
+				ILIntEncoder.encode(id, out);
+				ILIntEncoder.encode(ILTag.MAX_TAG_SIZE + 1, out);
+			}
+			ByteBufferDataInput in = new ByteBufferDataInput(ByteBuffer.wrap(bOut.toByteArray()));
+			assertThrows(TagTooLargeException.class, () -> {
+				h.deserialize(in);
+			});
+		}
+	}
+
+	@Test
+	void testDeserializeHeader() throws Exception {
+
+		// Implicit
+		for (long id = 0; id < 16; id++) {
+			ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+			try (DataOutputStream out = new DataOutputStream(bOut)) {
+				ILIntEncoder.encode(id, out);
+			}
+			ByteBufferDataInput in = new ByteBufferDataInput(ByteBuffer.wrap(bOut.toByteArray()));
+			ILTagHeader h = ILTagHeader.deserializeHeader(in);
+			assertEquals(id, h.tagId);
+			assertEquals(TagID.getImplicitValueSize(id), h.valueSize);
+		}
+
+		// General
+		for (long id : SAMPLE_IDS) {
+			for (long size : SAMPLE_SIZES) {
+				ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+				try (DataOutputStream out = new DataOutputStream(bOut)) {
+					ILIntEncoder.encode(id, out);
+					ILIntEncoder.encode(size, out);
+				}
+				ByteBufferDataInput in = new ByteBufferDataInput(ByteBuffer.wrap(bOut.toByteArray()));
+				ILTagHeader h = ILTagHeader.deserializeHeader(in);
+				assertEquals(id, h.tagId);
+				assertEquals(size, h.valueSize);
+			}
+		}
+
+		for (long id : SAMPLE_IDS) {
+			ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+			try (DataOutputStream out = new DataOutputStream(bOut)) {
+				ILIntEncoder.encode(id, out);
+				ILIntEncoder.encode(ILTag.MAX_TAG_SIZE, out);
+			}
+			ByteBufferDataInput in = new ByteBufferDataInput(ByteBuffer.wrap(bOut.toByteArray()));
+			ILTagHeader h = ILTagHeader.deserializeHeader(in);
+			assertEquals(id, h.tagId);
+			assertEquals(ILTag.MAX_TAG_SIZE, h.valueSize);
+		}
+
+		for (long id : SAMPLE_IDS) {
+			ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+			try (DataOutputStream out = new DataOutputStream(bOut)) {
+				ILIntEncoder.encode(id, out);
+				ILIntEncoder.encode(ILTag.MAX_TAG_SIZE + 1, out);
+			}
+			ByteBufferDataInput in = new ByteBufferDataInput(ByteBuffer.wrap(bOut.toByteArray()));
+			assertThrows(TagTooLargeException.class, () -> {
+				ILTagHeader.deserializeHeader(in);
+			});
+		}
+	}
+
+}
